35137530d75f55a67bb7873c955600a1aacb3f1c
wulujunwlj <wulujunwlj@163.com>
2015-05-31 21:56:04 +0800

    ds and NodeJS

---
+++ 7-days-nodejs/NodeJS/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var counter1 = require('./util/counter');
+var counter2 = require('./util/counter.js');
+
+console.log(counter1.count());
+console.log(counter2.count());
+console.log(counter2.count());
---
+++ 7-days-nodejs/NodeJS/util/counter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var i = 0;
+
+function count() {
+	return ++i;
+};
+
+exports.count = count;
