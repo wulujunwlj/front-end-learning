1a9659beb6cf845562879edf426ac8d5be31c15c
wulujunwlj <wulujunwlj@163.com>
2016-02-03 11:11:20 +0800

    add yuidocjs

---
+++ demos/yui-doc/index.js
@@ -0,0 +1,22 @@
+/**
+ * This is the description of my class
+ * 
+ * @class  MyClass
+ * @constructor
+ */
+(function(window) {
+	'use strict';
+
+	/**
+	 * My method description.  Like other pieces of your comment blocks, 
+	 * this can span multiple lines.
+	 *
+	 * @method adder
+	 * @param  {[type]} add1 [description]
+	 * @param  {[type]} add2 [description]
+	 * @return {[type]}      [description]
+	 */
+	function adder(add1, add2) {
+		return add1 + add2;
+	}
+})(window);
