13073aaee699b7ad98fe70372351b7c404ac5ad9
wulujunwlj <wulujunwlj@163.com>
2016-03-08 19:42:21 +0800

    update grunt

--- demos/ksh-grunt-generator/Gruntfile.js
+++ demos/ksh-grunt-generator/Gruntfile.js
@@ -1,13 +1,43 @@
-'use strict';
 
+// wrapper 函数
 module.exports = function (grunt) {
+	'use strict';
+
+	// 配置
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+		    options: {
+		    	mangle: false, 
+		        stripBanners: true,
+		        banner: '/*! <%= pkg.name %> - <%= pkg.version %>.js <%= grunt.template.today("yyyy-mm-dd")%> */\n'
+		    },
+		    build: {
+		        src: 'src/*.js',
+		        dest: 'build/<%= pkg.name %>-<%= pkg.version %>.min.js'
+		    }
+		}, 
+
 		jshint: {
-			files: 'src/*.js'
+		    build: ['Gruntfile.js', 'src/*.js'],
+		    options: {
+		        jshintrc: '.jshintrc'
+		    }
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	// 加载插件
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', []);
+	// 注册任务
+	// grunt.registerTask('default', 'This is the default task...', function() {
+	// 	grunt.log.writeln('Default task is running...');
+
+	// 	grunt.log.writeln(Array.prototype.slice(arguments).join(','));
+	// });
+
+	grunt.registerTask('default', ['uglify']);
+
+	// grunt.registerTask('uglify', ['uglify']);
 };
