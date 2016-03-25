7d7d4e0db11e5adc818ffaef323f2de42dca5cb0
wulujunwlj <wulujunwlj@163.com>
2016-03-25 17:15:38 +0800

    add py-book

---
+++ FE-skills/others/python/books/LPTHW/exercise1.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8-*-*
+
+print 'Hello World!'
+print 'Hello again'
+print 'I like typing this'
+print 'This is fun'
+# print 'After fun'
+print 'Yay! Printing'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.我是中文'
+print '这里是习题练习'
+# 
